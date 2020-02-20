7f47c248937a7322cfd7008b2977f3ecb1684ac5
PrasadTelasula <prasad.telasula@yahoo.com>
2020-02-21 01:03:19 +0530

    Update chroot_jail.sh

--- chroot_jail.sh
+++ chroot_jail.sh
@@ -7,42 +7,59 @@ echo
 read -n 1 -s -r -p ""
 sudo tree /bashjail
 echo
+
 read -n 1 -s -r -p ""
 echo "+ sudo mkdir /bashjail/{bin,etc}"
 sudo mkdir /bashjail/{bin,etc}
 echo
+
 read -n 1 -s -r -p ""
 sudo tree /bashjail
+
 read -n 1 -s -r -p ""
 echo
 echo "+ ldd /bin/bash"
 ldd /bin/bash
 echo
+
 read -n 1 -s -r -p ""
 echo "+ ldd /bin/ls"
 ldd /bin/ls
 echo
+
+
 read -n 1 -s -r -p ""
 echo "+ sudo mkdir -p /bashjail/{lib64,lib/x86_64-linux-gnu}"
 sudo mkdir -p /bashjail/{lib64,lib/x86_64-linux-gnu}
 echo
+
+
 read -n 1 -s -r -p ""
 sudo tree /bashjail
 echo
+
+
 read -n 1 -s -r -p ""
 echo "+ sudo cp /bin/bash /bashjail/bin"
 sudo cp /bin/bash /bashjail/bin
+
+
 read -n 1 -s -r -p ""
 echo
 sudo tree /bashjail
+
 read -n 1 -s -r -p ""
 echo
 echo "+ sudo cp /bin/ls /bashjail/bin"
 echo
 sudo cp /bin/ls /bashjail/bin
+
+
 read -n 1 -s -r -p ""
 echo
 sudo tree /bashjail
+
+
 read -n 1 -s -r -p ""
 echo "PS1='JAIL $ '" | sudo tee /bashjail/etc/bash.bashrc
 echo
@@ -79,12 +96,15 @@ do \
 	sudo cp -v $i /bashjail/lib/x86_64-linux-gnu/; \
 done
 echo
+
 read -n 1 -s -r -p ""
 echo
 sudo tree /bashjail
 read -n 1 -s -r -p ""
 echo
 echo "+ sudo cp /lib64/ld-linux-x86-64.so.2 /bashjail/lib64/"
+
+
 read -n 1 -s -r -p ""
 sudo cp /lib64/ld-linux-x86-64.so.2 /bashjail/lib64/
 echo
